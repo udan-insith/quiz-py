bc44fba8c948c4b0953f53ec582306fd69dfd2d5
udan-insith <udaninsith23613@gmail.com>
2026-04-08 10:45:49 +0530

    Update quiz_game.py

--- quiz_game.py
+++ quiz_game.py
@@ -1,3 +1,11 @@
 print("Welcome to my computer quiz!")
 
-playing = input("Do you want to play? ")
+playing = input("Do you want to play(yes/no)? ")
+
+if (playing != "yes"):
+    quit()
+
+print("Okay! Let's play :)")
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("correct!")
